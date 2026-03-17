13b15dc63be31f083e42d06563ae6f29094cd18e
madan21-ui <mehunmadan@gmail.com>
2026-03-17 21:40:31 +0500

    import dotenv

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,15 @@
 //const express = require("express")  => old syntax
 import express from "express"
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("It's working ");
 });
+
+console.log("my port", process.env.PORT);
 
 app.listen(5001, () => {
     console.log("Server is up and running on PORT:5001");
