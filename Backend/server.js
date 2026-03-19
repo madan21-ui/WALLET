bd44d461ede58816414667ad297f62e2cb572cc4
madan21-ui <mehunmadan@gmail.com>
2026-03-19 22:09:49 +0500

    working with server.js

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+async function  initDB(){
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        )` 
+    } catch (error) {
+        
+    }
+}
+
 connectDB(process.env.DATABASE_URL)
 
 app.get("/", (req, res) => {
