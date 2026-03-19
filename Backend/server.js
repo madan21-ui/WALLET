5ee2cadb9405f598cf6a033b1198a8d14cbcaecc
madan21-ui <mehunmadan@gmail.com>
2026-03-19 22:15:27 +0500

    working

--- Backend/server.js
+++ Backend/server.js
@@ -12,9 +12,14 @@ async function  initDB(){
         id SERIAL PRIMARY KEY,
         user_id VARCHAR(255) NOT NULL,
         title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10,2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created DATE NOT NULL DEFAULT  CURRENT_DATE,
         )` 
+
+        console.log("Database initialized successfully")
     } catch (error) {
-        
+        console.log("Error initializing DB")
     }
 }
 
