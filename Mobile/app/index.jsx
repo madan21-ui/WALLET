78afef34edb9e7ece4ae58a68d17cc99b54324e5
madan21-ui <mehunmadan@gmail.com>
2026-03-27 17:22:01 +0500

    styles for auth added

--- Mobile/app/index.jsx
+++ Mobile/app/index.jsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View , StyleSheet} from "react-native";
 import about from "./about";
 import { Link } from "expo-router";
 import {Image} from "expo-image"
@@ -6,14 +6,22 @@ import {Image} from "expo-image"
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Image source={URL="https://tse3.mm.bing.net/th/id/OIP.bsUst1JNh5eBlO8DuVdVKAHaDO?w=1380&h=600&rs=1&pid=ImgDetMain&o=7&rm=3"} style={{width:100,height:100}}></Image>
+      style={style.container}>
+      <Text>Madan</Text>
+      
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image:{
+    width: "50%",
+    height: "50%"
+
+  }
+})
