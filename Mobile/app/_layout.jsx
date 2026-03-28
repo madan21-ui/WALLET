fa77f2bc96b9264221790131475262fbb2cd9ecd
madan21-ui <mehunmadan@gmail.com>
2026-03-28 17:35:08 +0500

    issue

--- Mobile/app/_layout.jsx
+++ Mobile/app/_layout.jsx
@@ -1,14 +1,17 @@
-import { Stack,Slot } from "expo-router";
-import SafeScreen from "@/component/SafeScreen";
 import { ClerkProvider } from '@clerk/expo'
 import { tokenCache } from '@clerk/expo/token-cache'
+import { Slot } from 'expo-router'
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 export default function RootLayout() {
-  return <ClerkProvider ublishableKey={publishableKey} tokenCache={tokenCache}>
-    <SafeAreaView>
-    <Slot/>
-
-    </SafeAreaView>
-  </ClerkProvider>
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <Slot />
+    </ClerkProvider>
+  )
 }
